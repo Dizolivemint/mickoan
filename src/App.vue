f55e2df0b82845743d35d02fe9d49ffeb780c234
Miles Exner <dizolivemint@gmail.com>
2019-03-16 15:27:27 -0700

    [Update] Move post data into a prop

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view :posts="posts"/>
   </div>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    posts: [
+      { 
+        id: 0,
+        content: 'An astronaut watches an asteroid and comments "paint job needs to be fixed". A voice echos "fixed". A woman watches.',
+        category: 'space' 
+      },
+      { 
+        id: 1,
+        content: 'An astronaut watches an asteroid and comments "paint job needs to be fixed". A voice echos "fixed". A woman watches.',
+        category: 'space' 
+      }
+    ]
+  })
+}
+</script>
+
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(x, index) in 2" :key="index" :class="`bg-strips strip-${index}`"></div>
+    <div class="bg-color-overlay"></div>
+    <Header></Header>
+     <!-- Main Content -->
+    <div v-for="(post, index) in posts" :key="index" class="m-4 card">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <p  class="mt-4 p-3 post">
+            {{ post.content }}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+    <Footer></Footer>
+  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/root/Header.vue'
+import Footer from '@/components/root/Footer.vue'
 
 export default {
   name: 'home',
+  props: ['posts'],
   components: {
-    HelloWorld
-  }
+    Header,
+    Footer
+  },
+  data: () => ({})
 }
 </script>
+
+<style lang="scss">
+  .card {
+    -webkit-box-shadow: -3px 5px 30px -9px rgba(0,0,0,1);
+    -moz-box-shadow: -3px 5px 30px -9px rgba(0,0,0,1);
+    box-shadow: -3px 5px 30px -9px rgba(0,0,0,1);
+  }
+  .bg-strips {
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+  }
+  .strip-0 {
+    background-color: rgb(97, 0, 0);
+    -webkit-clip-path: polygon(50% 5%, 61% 35%, 95% 35%, 68% 57%, 80% 95%, 50% 70%, 20% 95%, 32% 57%, 5% 35%, 39% 35%);
+    clip-path: polygon(50% 5%, 61% 35%, 95% 35%, 68% 57%, 80% 95%, 50% 70%, 20% 95%, 32% 57%, 5% 35%, 39% 35%);
+  }
+  .strip-1 {
+    background-color: rgb(16, 12, 56);
+    -webkit-clip-path: polygon(0% 0%, 0% 100%, 5% 100%, 5% 5%, 95% 5%, 95% 95%, 5% 95%, 5% 100%, 100% 100%, 100% 0%);
+    clip-path: polygon(0% 0%, 0% 100%, 5% 100%, 5% 5%, 95% 5%, 95% 95%, 5% 95%, 5% 100%, 100% 100%, 100% 0%);
+  }
+  .bg-color-overlay {
+    opacity: .1;
+    background-color: #97a4df;
+    background-attachment: fixed;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+  }
+  Footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
